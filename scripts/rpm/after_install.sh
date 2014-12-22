eaa166e1ba3ad800fa6583cfb590f5e372d6fb13
Damian Kaczmarek <rush@rushbase.net>
2014-12-22 12:11:30 +0100

    Open firewall and auto start services

--- scripts/rpm/after_install.sh
+++ scripts/rpm/after_install.sh
@@ -14,3 +14,19 @@ systemctl enable sshd
 . setup/system.sh
 chown -R virtkick-run:root /opt/virtkick
 
+if which firewall-cmd > /dev/null; then
+  echo "Opening port 3000 on zone public"
+  firewall-cmd --zone=public --add-port=3000/tcp --permanent
+  firewall-cmd --reload
+fi
+
+for service in proxy webapp backend; do
+  systemctl enable virtkick-$service
+  systemctl start virtkick-$service
+done
+
+for n in 1 2 3 4 5; do
+  systemctl enable virtkick-work@$n
+  systemctl start virtkick-work@$n
+done
+
